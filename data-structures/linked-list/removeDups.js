831aeaf7e700a1761c5b16d6490c5a1abca7b8e1
Jane Fong <janeefongg@gmail.com>
2016-07-03 19:37:33 -0700

    [new] remove duplicates linked list

--- data-structures/linked-list/removeDups.js
+++ data-structures/linked-list/removeDups.js
@@ -1,1 +1,28 @@
 // implement a function that removes duplicates from an unsorted linked list
+
+//method 1: store into linked list. if collision, remove node
+  //time complexity: O(N) time for N number of elements in list
+//method 2: iterate nodes and have another iterator to find duplicates
+  //time complexity: O(N2) time, but O(1) space
+
+
+//method 2:
+function removeDups (linkedlist) {
+  var current = linkedlist;
+  
+  while (current) {
+    var pointer = current;
+    while(pointer.next) {
+      if (pointer.next.value === current.value) {
+        pointer.next = pointer.next.next;
+      } else {
+        pointer = pointer.next;
+      }
+    }
+    current = current.next;
+  }
+  
+  return linkedlist;
+}
+
+
