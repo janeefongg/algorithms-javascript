e6c38361ff8d291dc6f0ae41ed947b75c0a4ffba
janeefongg <janeefongg@gmail.com>
2016-07-04 21:17:52 -0700

    [new] binary search array

---
+++ data-structures/binarySearchArray.js
@@ -0,0 +1,21 @@
+//implement an algorithm that searches through a sorted array for a target
+//if target found, return index. else return -1
+
+function binarySearchArray (array, target) {
+    var min = 0;
+    var max = array.length - 1;
+    var mid;
+
+    while (min <= max) {
+        mid = Math.floor((min + max) / 2);
+        if (array[mid] === target) {
+            return mid;
+        } else if (target < array[mid]) {
+            max = mid - 1;
+        } else if (target > array[mid]) {
+            min = mid + 1;
+        }
+    }
+
+    return -1;
+}
