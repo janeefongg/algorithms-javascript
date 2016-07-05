9e84e165950d9a1e44e5f0d2533dbeb1b78084f8
janeefongg <janeefongg@gmail.com>
2016-07-04 22:14:10 -0700

    [new] merge sort

--- sorting-algorithms/mergeSort.js
+++ sorting-algorithms/mergeSort.js
@@ -4,3 +4,34 @@ Notes:
 -Time Complexity: O(nlog2n)
   -This is from the height of the tree to the number of nodes
  */
+
+function mergeSort (array) {
+    if (array.length < 2) {
+        return array;
+    }
+    
+    var mid = parseInt(array.length / 2);
+    var left = array.slice(0, mid);
+    var right = array.slice(mid);
+    
+    return merge(mergeSort(left), mergeSort(right));
+};
+
+function merge (left, right) {
+    var results = [];
+    
+    while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+            results.push(left.shift());
+            results.push(right.shift());
+        }
+    }
+    
+    while (left.length) {
+        results.push(left.shift());
+        results.push(right.shift());
+    }
+    
+    return results;
+}
+
