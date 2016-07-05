d0c879f0f54930e932c2b99d09769c96aa2220e3
janeefongg <janeefongg@gmail.com>
2016-07-04 21:25:41 -0700

    [new] binary search tree implementation

--- data-structures/binarySearchTree.js
+++ data-structures/binarySearchTree.js
@@ -1,3 +1,54 @@
 /*
 A binary tree where the left child is lesser than the parent and the right child is greater than the parent
  */
+
+var BinarySearchTree = function (value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+};
+
+BinarySearchTree.prototype.insert = function (value) {
+    if (value < this.value) {
+        if (this.left === null) {
+            this.left = BinarySearchTree(value);
+        } else {
+            this.left.insert(value);
+        }
+    } else if (value > this.value) {
+        if (this.right === null) {
+            this.right = BinarySearchTree(value);
+        } else {
+            this.right.insert(value);
+        }
+    }
+};
+
+BinarySearchTree.prototype.contains = function (value) {
+    if (this.value === value) {
+        return true;
+    } else if (value < this.value) {
+        if (!this.left) {
+            return false;
+        } else {
+            return this.left.contains(value);
+        }
+    } else if (value > this.value) {
+        if (!this.right) {
+            return false;
+        } else {
+            return this.right.contains(value);
+        }
+    }
+};
+
+BinarySearchTree.prototype.depthFirstLog = function (callback) {
+    callback(this.value);
+    if (this.left) {
+        this.left.depthFirstLog(callback);
+    }
+    
+    if (this.right) {
+        this.right.depthFirstLog(callback);
+    }
+};
