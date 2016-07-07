bbfb0da526e47ff523606b26a29f80b5ef284153
Jane Fong <janeefongg@gmail.com>
2016-07-06 20:17:17 -0700

    [test] checkPermutation and isUnique

--- data-structures/arrays-strings/isUnique.js
+++ data-structures/arrays-strings/isUnique.js
@@ -18,7 +18,7 @@ Psuedocode (without additional data structures):
 //with additional data structure
 //time complexity: o(n^2) because of for loop and indexof
 
-function isUnique (string) {
+module.exports.isUnique = function (string) {
   var strArr = [];
 
   for (var i = 0; i < string.length; i++) {
@@ -31,11 +31,10 @@ function isUnique (string) {
 
 //without additional data structure
 //time complexity: o(n^2) because of for loop with inner for loop
-function isUnique2 (string) {
+module.exports.isUnique2 = function (string) {
   for (var i = 0; i < string.length; i++) {
     for (var j = i + 1; j < string.length; j++) {
       if (string.charAt(i) === string.charAt(j)) {
-        console.log('j', j)
         string = string.split('')
         string.splice(j, 1)
         string = string.join('');
--- test/dataStructureSpec.js
+++ test/dataStructureSpec.js
@@ -1,11 +1,19 @@
 var expect = require('expect.js');
 
 var checkPermutation = require('../data-structures/arrays-strings/checkPermutation').checkPermutation;
+var isUnique = require('../data-structures/arrays-strings/isUnique').isUnique;
+var isUnique2 = require('../data-structures/arrays-strings/isUnique').isUnique2;
 
 describe('Arrays and Strings', function(){
   it('should check if a string is a permutation of the other', function(){
 
     expect(checkPermutation('ayy', 'yay')).to.be(true);
+  });
+
+  it('should remove duplicates from an input string', function(){
+
+    expect(isUnique('banana', 'ban')).to.equal('ban');
+    expect(isUnique2('banana', 'ban')).to.equal('ban');
   });
 
 });
