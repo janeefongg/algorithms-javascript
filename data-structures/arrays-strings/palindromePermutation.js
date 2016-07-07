4a2601b6d6d1d7556ca6e2d1e7c18d4fa407bffe
Jane Fong <janeefongg@gmail.com>
2016-07-06 18:28:21 -0700

    [setup] algorithm instructions

---
+++ data-structures/arrays-strings/palindromePermutation.js
@@ -0,0 +1,3 @@
+/*
+Given a string, write a function to check if it is a permutation of a palindrome.
+ */
---
+++ data-structures/arrays-strings/stringCompression.js
@@ -0,0 +1,6 @@
+/*
+Implement a method to perform basic string compression using the counts of repeated characters.
+
+Input: 'aabcccccaaa'
+Output: 'a2b1c5a3'
+ */
