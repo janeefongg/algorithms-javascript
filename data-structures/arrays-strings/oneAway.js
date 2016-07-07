090e0de41bae02129cb0ff32f85b84b4d3956cba
Jane Fong <janeefongg@gmail.com>
2016-07-07 13:28:19 -0700

    [new] start on oneAway function

--- data-structures/arrays-strings/oneAway.js
+++ data-structures/arrays-strings/oneAway.js
@@ -6,3 +6,24 @@ There are three types of edits that can be performed on strings:
  
 Given two strings, write a function to check if they are one edit (or zero edits) away.
  */
+
+module.exports.oneAway = function (input1, input2) {
+  //check if second input length is smaller than the other
+    //if it is, find the letter and its index
+    //splice it into the second input
+    //compare
+  //if first input is smaller than the second input
+    //find the extra letter and its index
+    //splice into the first input
+    //compare
+  //if lengths are equal
+    //iterate through and count the differences
+    //if difference count is greater than 1, return false
+  if (input1.length > input2.length) {
+    
+  } else if (input1.length < input2.length) {
+    
+  } else {
+    
+  }
+};
--- test/dataStructureSpec.js
+++ test/dataStructureSpec.js
@@ -4,6 +4,7 @@ var checkPermutation = require('../data-structures/arrays-strings/checkPermutati
 var isUnique = require('../data-structures/arrays-strings/isUnique').isUnique;
 var isUnique2 = require('../data-structures/arrays-strings/isUnique').isUnique2;
 var urlify = require('../data-structures/arrays-strings/urlify').urlify;
+var oneAway = require('../data-structures/arrays-strings/oneAway').oneAway;
 
 describe('Arrays and Strings', function(){
   it('should check if a string is a permutation of the other', function(){
@@ -20,6 +21,13 @@ describe('Arrays and Strings', function(){
   it('should fill the empty spaces in an input string with %20', function () {
     expect(urlify('Mr John Smith   ', 13)).to.be('Mr%20John%20Smith');
     expect(urlify('Hello, Jane!', 12)).to.be('Hello,%20Jane!');
+  })
+
+  it ('should check if an input is one edit away from being the same as the second input', function () {
+    expect(oneAway('pale', 'ple')).to.be(true);
+    expect(oneAway('pales', 'pale')).to.be(true);
+    expect(oneAway('pale', 'bale')).to.be(true);
+    expect(oneAway('pale', 'bake')).to.be(false);
   })
 
 });
