29c0cd4af8d533023bf9d3a0936de76ccc986e9c
Jane Fong <janeefongg@gmail.com>
2016-07-07 16:50:40 -0700

    [new] stringCompression algorithm with passing test specs

--- data-structures/arrays-strings/stringCompression.js
+++ data-structures/arrays-strings/stringCompression.js
@@ -4,3 +4,26 @@ Implement a method to perform basic string compression using the counts of repea
 Input: 'aabcccccaaa'
 Output: 'a2b1c5a3'
  */
+
+module.exports.stringCompression = function (input) {
+  var results = [];
+  var counter = 0;
+  var starter;
+
+  for (var i = 0; i < input.length; i++) {
+    if (!starter) {
+      starter = input[i];
+      counter++;
+    } else if (input[i] === starter) {
+      counter++;
+    } else {
+      results.push(starter + counter);
+      starter = input[i];
+      counter = 1;
+    }
+  }
+
+  results.push(starter+counter);
+  return results.join('');
+};
+
--- test/dataStructureSpec.js
+++ test/dataStructureSpec.js
@@ -5,6 +5,7 @@ var isUnique = require('../data-structures/arrays-strings/isUnique').isUnique;
 var isUnique2 = require('../data-structures/arrays-strings/isUnique').isUnique2;
 var urlify = require('../data-structures/arrays-strings/urlify').urlify;
 var oneAway = require('../data-structures/arrays-strings/oneAway').oneAway;
+var stringCompression = require('../data-structures/arrays-strings/stringCompression').stringCompression;
 
 describe('Arrays and Strings', function(){
   it('should check if a string is a permutation of the other', function(){
@@ -21,13 +22,17 @@ describe('Arrays and Strings', function(){
   it('should fill the empty spaces in an input string with %20', function () {
     expect(urlify('Mr John Smith   ', 13)).to.be('Mr%20John%20Smith');
     expect(urlify('Hello, Jane!', 12)).to.be('Hello,%20Jane!');
-  })
+  });
 
   it ('should check if an input is one edit away from being the same as the second input', function () {
     expect(oneAway('pale', 'ple')).to.be(true);
     expect(oneAway('pales', 'pale')).to.be(true);
     expect(oneAway('pale', 'bale')).to.be(true);
     expect(oneAway('pale', 'bake')).to.be(false);
-  })
+  });
+  
+  it('should compress a string and return a new string with the letter and its count', function () {
+    expect(stringCompression('aabccccca')).to.equal('a2b1c5a1');
+  });
 
 });
