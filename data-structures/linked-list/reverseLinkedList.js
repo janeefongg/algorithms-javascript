74f5ea4a6111d87c0d54855b6b687a5989ff8d3b
Jane Fong <janeefongg@gmail.com>
2016-07-09 21:26:49 -0700

    [setup] instructions for reverse linked list added

--- data-structures/linked-list/reverseLinkedList.js
+++ data-structures/linked-list/reverseLinkedList.js
@@ -1,3 +1,7 @@
+/*
+Given the head of a linked list, implement an algorithm that reverses the order of the list and returns new head
+ */
+
 function reverseList (head) {
   var prev = null;
   var current = head;
