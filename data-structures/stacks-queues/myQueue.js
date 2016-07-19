96ffde8bcb86bab962c9ff22a40cfc72962d93bf
Jane Fong <janeefongg@gmail.com>
2016-07-19 15:01:26 -0700

    [new] implement queue with two stacks

--- data-structures/stacks-queues/myQueue.js
+++ data-structures/stacks-queues/myQueue.js
@@ -1,1 +1,29 @@
 //implement MyQueue class that implements a queue using two stacks
+'use strict'
+
+var Queue = function () {
+  var inbox = new Stack();
+  var outbox = new Stack();
+
+  this.enqueue = function (value) {
+    inbox.push(value);
+  };
+
+  this.dequeue = function (value) {
+    var temp;
+    while (inbox.size() > 0) {
+      outbox.push(inbox.pop());
+    };
+
+    temp = outbox.pop();
+
+    while (outbox.size() > 0) {
+      inbox.push(outbox.pop());
+    }
+    return temp;
+  };
+
+  this.size = function () {
+    return inbox.size();
+  };
+}
